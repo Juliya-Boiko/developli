0677299caae59957ff500525bb7043aee236b5c4
Juliya Boiko <boikojuliya2707@gmail.com>
2024-06-04 10:00:47 +0300

    add Process

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Hero } from "@/components/sections/hero"
-
+import { Process } from "@/components/sections/process";
 export default function Home() {
   return (
     <>
       <Hero />
+      <Process />
     </>
   );
 }
