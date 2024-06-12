9113b18c549910c7cf5f3cb093701d508e503b96
Juliya Boiko <boikoj@ou.org>
2024-06-12 14:31:36 +0300

    add privacy-policy page

--- src/components/sections/policy/index.tsx
+++ src/components/sections/policy/index.tsx
@@ -8,10 +8,9 @@ import { SelectOption } from './type'
 
 const Select = dynamic(() => import('react-select'), { ssr: false })
 
-const options = policyTerms.map((el) => ({ value: el.title, label: el.title }))
+const options: SelectOption[] = policyTerms.map((el) => ({ value: el.title, label: el.title }))
 
 export const PrivacyConditions = () => {
-
   const [selectedOption, setSelectedOption] = useState<SingleValue<SelectOption>>(options[0])
   const [isClient, setIsClient] = useState(false)
 
@@ -26,9 +25,19 @@ export const PrivacyConditions = () => {
   const renderContent = () => {
     const selectedItem = policyTerms.find((item) => item.title === selectedOption?.value);
     return selectedItem ? (
-      <div>
-        <h2>{selectedItem.title}</h2>
-        <p>{selectedItem.text}</p>
+      <div className={styles.wrapper}>
+        <h2 className={styles.termName}>{selectedItem.title}</h2>
+        <p className={styles.termDefinition}>{selectedItem.text}</p>
+        <ul>
+          {selectedItem.terms.map((el, idx) => (
+            <li key={`${el.title}-${idx}`}>
+              <p className={styles.termTitle}>{el.title}</p>
+              {el.text.map((str) => (
+                <p key={str} className={styles.termText}>{str}</p>
+              ))}
+            </li>
+          ))}
+        </ul>
       </div>
     ) : null;
   };
@@ -37,12 +46,39 @@ export const PrivacyConditions = () => {
     <section className={`container ${styles.section}`}>
       <h1 className={styles.title}>Legal Information</h1>
       <p className={styles.subTitle}>If you have any questions you can report them below with the following information, so that it is easy for us to reply to your message.</p>
-      <div>
+      <div className={styles.content}>
         <Select
           defaultValue={selectedOption}
           onChange={handleChange}
           options={options}
+          styles={{
+            control: base => ({
+              ...base,
+              border: 0,
+              boxShadow: 'none',
+            }),
+            option: (styles, { isSelected }) => ({
+              ...styles,
+              backgroundColor: isSelected ? '#47D18C' : 'white'
+            })
+          }}
+          className={styles.select}
         />
+        <div className={styles.termsWrapper}>
+          {options.map((el) => (
+            <button
+              type='button'
+              key={el.value}
+              className={styles.termBtn}
+              style={{
+                color: el.value === selectedOption?.value ? '#47D18C' : '#252A3F'
+              }}
+              onClick={() => handleChange(el)}
+            >
+              {el.value}
+            </button>
+          ))}
+        </div>
         {renderContent()}
       </div>
     </section>
